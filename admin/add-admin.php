8cdabad10c8350ebbb5e10f067fd45820c5e05a5
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-09-16 23:23:13 +0400

    updated add-admin page, added session

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -3,33 +3,41 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Add Admin</h1>
-        <br>
-        <br>
+
+        <br><br>
+
+        <?php 
+            if(isset($_SESSION['add'])){
+                echo $_SESSION['add']; 
+                unset($_SESSION['add']); 
+            }
+        ?>
+
         <form action="" method="POST">
             <table class="tbl-30">
                 <tr>
                     <td>Full Name: </td>
                     <td>
-                        <input type="text" name="full_name" placeholder="Enter your name">
+                        <input type="text" name="full_name" placeholder="Enter Your Name">
                     </td>
                 </tr>
 
                 <tr>
                     <td>Username: </td>
                     <td>
-                        <input type="text" name="username" placeholder="Enter your username">
+                        <input type="text" name="username" placeholder="Your Username">
                     </td>
                 </tr>
 
                 <tr>
                     <td>Password: </td>
                     <td>
-                        <input type="password" name="password" placeholder="Enter your password">
+                        <input type="password" name="password" placeholder="Your Password">
                     </td>
                 </tr>
 
                 <tr>
-                    <td colspan="">
+                    <td colspan="2">
                         <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
                     </td>
                 </tr>
@@ -40,28 +48,31 @@
 
 <?php include('partials/footer.php'); ?>
 
+
 <?php 
-    if(isset($_POST['submit'])){
-        //Get data
+    if(isset($_POST['submit'])) {
+
         $full_name = $_POST['full_name'];
         $username = $_POST['username'];
-        $password = md5($_POST['password']); // for hash password, sra urish tarberakn el ka
+        $password = md5($_POST['password']); //sra aveli lav tarberakn el ka
 
-        //Query for save database
-        $sql = "INSERT INTO tbl_admin SET
-            full_name = '$full_name',
-            username = '$username',
-            password = '$password'
+        $sql = "INSERT INTO tbl_admin SET 
+            full_name='$full_name',
+            username='$username',
+            password='$password'
         ";
-
+ 
         $res = mysqli_query($conn, $sql) or die(mysqli_error());
 
         if($res==TRUE){
-            $SESSION['add'] = "Success";
-            //connect to manage admine page
+            $_SESSION['add'] = "<div class='success'>Admin Added Successfully.</div>"; 
+            //Connect to manage admin page
             header("location:".SITEURL.'admin/manage-admin.php');
-        } else {
-            // echo "failed insert data";
         }
-    };
+        else{
+            
+            $_SESSION['add'] = "<div class='error'>Failed to Add Admin.</div>";
+            header("location:".SITEURL.'admin/add-admin.php');
+        }
+    } 
 ?>
